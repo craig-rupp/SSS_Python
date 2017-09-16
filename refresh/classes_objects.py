5cfd9f5a9df61a67f94f5e630324e755cd37d524
Craig Rupp <craigrupp@utexas.edu>
2017-09-16 14:01:42 -0600

    Exercise 26 Store Object with list comprehension

--- refresh/classes_objects.py
+++ refresh/classes_objects.py
@@ -25,3 +25,40 @@ craig = Student('Craig', 'UT')
 print(craig.name + ', went to ' + craig.school)
 craig.grades.extend((75, 89, 91))
 print(craig.grade_average())
+
+#Exercise #26
+class Store:
+    def __init__(self, name):
+        self.name = name
+        self.items = []
+
+    def add_item(self, name, price):
+        # Create a dictionary with keys name and price, and append that to self.items.
+        item_dictionary = {
+            'name' : name,
+            'price' : price
+        }
+        self.items.append(item_dictionary)
+
+    def add_items(self, items):
+        self.items.extend(items)
+        print(self.items)
+
+    def stock_price(self):
+    # Add together all item prices in self.items and return the total.
+        print(sum([item['price'] for item in self.items]))
+
+test_store = Store('Albertsons')
+#test_store.add_item('Face-Wash', 5)
+items = [
+    {
+        'name' : 'Cerveza',
+        'price' : 25
+    },
+    {
+        'name' : 'Tecate',
+        'price' : 18
+    }
+]
+test_store.add_items(items)
+test_store.stock_price()
