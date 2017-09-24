81039fd3797a7e5e45525f2127515610d17e7d08
Craig Rupp <craigrupp@utexas.edu>
2017-09-24 11:33:22 -0600

    Initial REST endpoints

--- first_flask/initial_app.py
+++ first_flask/initial_app.py
@@ -2,6 +2,17 @@ from flask import Flask
 ##package is the lower case, class is upper clase
 
 app = Flask(__name__)
+stores = [
+    {
+        'name': 'Yo-Store',
+        'items': [
+            {
+                'name': 'Item_1',
+                'price': 15
+            }
+        ]
+    }
+]
 
 ##__name__ gives each a unique name
 
@@ -17,3 +28,31 @@ app.run(port=5000)
 # POST - Server receives data (we sent), and use it
 # PUT - Make sure something is there ( PUT /item)
 # DELETE - Remove Something DELETE ( /item/1)
+
+##Stateless REST principle - one request cannot depend on any other request
+# server only knows about current request, not any other previous request
+
+# POST /store_data {name:}
+@app.route('/store', methods=['POST'])
+def create_store():
+    pass
+
+# GET /store/<string:name>
+@app.route('/store/<string:name>') ##<> flask syntax
+def return_single_store(name):
+    pass
+
+# GET /store
+@app.route('/store')
+def return_all():
+    pass
+
+# POST /store/<string:name>/item {name:price}
+@app.route('/store/<string:name>/item', methods=['POST'])
+def post_item(name):
+    pass
+
+# GET /store/<string:name>/item
+@app.route('/store/<string:name>/item')
+def return_store_item(name):
+    pass
