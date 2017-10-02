e3794192b1d05799856b632f4e2bca594c8d5bec
Craig Rupp <craigrupp@utexas.edu>
2017-10-02 16:21:57 -0600

    JWT library and security file from user class (new file for each)

--- Flask_RESTful/code/app.py
+++ Flask_RESTful/code/app.py
@@ -3,6 +3,7 @@ from flask_restful import Resource, Api
 
 
 app = Flask(__name__)
+app.secret_key = 'craig'
 api = Api(app)
 
 items = [
@@ -12,12 +13,17 @@ items = [
 
 class Item(Resource):
     def get(self, name):
-        for item in items:
-            if item['name'] == name:
-                return item
-        return {'item' : None}, 404
+        # for item in items:
+        #     if item['name'] == name:
+        #         return item
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        ## make sure to have None as available argument should next not return any filter
+        return {'item' : item}, 200 if item is not None else 404
 
     def post(self, name):
+        if next(filter(lambda x: x['name'] == name, items), None):
+            return{'message': "An item with a name '{}' already exists".format(name)}, 400
+
         data = request.get_json()
         item = {
             'name' : name,
