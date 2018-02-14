f57d3b6270362cf4b017a52c24c9fdcc10dfbaf2
Craig Rupp <craigrupp@utexas.edu>
2018-02-14 16:55:24 -0700

    Flexibility to menu

--- 18/Sect_5/students/app.py
+++ 18/Sect_5/students/app.py
@@ -1,35 +1,55 @@
+student_list = []
 def create_student():
     student_name = input("What's the student's name? : ")
     student_dictionary = {
         "name" : student_name,
         "marks" : []
     }
-    return student_dictionary
+    student_list.append(student_dictionary)
+    another = input("Would you like to add another student? : [Y/N]?").lower()
+    if another == 'y':
+        create_student()
+    return student_list
 
-def add_grades(student):
-    grades = input("Please enter the grades for your student, separated by a comma : ")
-    grades = grades.split(",")
-    for grade in grades:
-        student["marks"].append(float(grade))
-    print(student)
-    confirm(student)
+def add_grades(students):
+    for student in students:
+        print(student['name'])
+        grades = input("Please enter the grades for : {}, separated by a comma : ".format(student["name"]))
+        grades = grades.split(",")
+        for grade in grades:
+            student["marks"].append(float(grade))
+    student_list = students
+    return student_list
 
 def confirm(dict):
-    print(dict)
-    name = dict["name"]
-    grades = dict["marks"]
-    answer = ""
-    while answer not in ["y", "n"]:
-        answer = input("Confirm grades : " + str(grades) + ", for " + name + " : [Y/N]").lower()
-    if answer == "y":
+    for individual_student in dict:
+        name = individual_student["name"]
+        grades = individual_student["marks"]
+        answer = ""
+        while answer not in ["y", "n"]:
+            answer = input("Confirm grades : " + str(grades) + ", for " + name + " : [Y/N]").lower()
+        if answer == "y":
         print("You have confirmed grades : " + str(grades) + " for " + name)
-        return dict
-    elif answer != "y":
-        dict["marks"] = []
-        print(name + ", has had their grades removed")
-        print(dict)
-        return dict
+        average = input("Would you like to have an average of the student's grades? : [Y/N]").lower()
+        if average == "y":
+            calculate_student_average(dict)
+        elif answer != "y":
+            individual_student["marks"] = []
+            print(name + ", has had their grades removed")
+        
+def calculate_student_average(student):
+    # calculate average, set new key in dictionary for average, print out student
+    student_average = sum(student["marks"])/len(student["marks"])
+    student['average'] = student_average
+    print(student)
+
+def menu():
+    #student list for all students, grades per student, print list, exit
+    print(create_student())
+    print(add_grades(student_list))
+    confirm(student_list)
 
 
+menu()
 
-add_grades(create_student())
+#add_grades(create_student())
